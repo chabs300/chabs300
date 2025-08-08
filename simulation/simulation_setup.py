1f876120eca583a65341f8bed228898905566922
chabs300 <160239584+chabs300@users.noreply.github.com>
2025-08-08 13:23:48 +0200

    Delete simulation directory

--- simulation/simulation_setup.py
+++
@@ -1,8 +0,0 @@
-"""
-simulation_setup.py
-Author: Paul Pircher
-Date: August 8, 2025
-Description: This file stores the configuration parameters for the
-vehicle track interaction (VTI) simulations. It defines the parameters needed
-for the simulation process and enables easy modification of these parameters.
-"""
